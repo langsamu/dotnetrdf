0234288b5f9333f6ec72edced3c3d89488ca7f33
Samu Lang <langsamu@hotmail.com>
2025-05-11 20:57:13 +0100

    Start covering RDF collection list

--- Testing/dotNetRdf.Wrapping.Tests/RdfCollectionListTest.cs
+++ Testing/dotNetRdf.Wrapping.Tests/RdfCollectionListTest.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using VDS.RDF.Parsing;
@@ -9,73 +9,173 @@ public class RdfCollectionListTest
 {
     private readonly static NodeFactory factory = new();
     private readonly static INode s = factory.CreateUriNode(UriFactory.Create("http://example.com/s"));
+    private readonly static INode p = factory.CreateUriNode(UriFactory.Create("http://example.com/p"));
+    private readonly static string v = Guid.NewGuid().ToString();
+    private readonly static INode o = factory.CreateLiteralNode(v);
 
-    [Theory]
-    [InlineData(0)]
-    [InlineData(-1)]
-    public void IndexerGetBounds(int index)
+    private readonly Graph g;
+    private readonly SampleWrapper wrapper;
+
+    public RdfCollectionListTest()
     {
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
-
-        l.Invoking(l => l[index]).Should().Throw<ArgumentOutOfRangeException>();
-    }
-
-    [Fact]
-    public void IndexerSetNull()
-    {
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
-
-        l.Invoking(l => l[0] = null!).Should().Throw<ArgumentNullException>();
+        g = new();
+        wrapper = new SampleWrapper(s, g);
     }
 
     [Theory]
-    [InlineData(0)]
     [InlineData(-1)]
-    public void IndexerSetBounds(int index)
-    {
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
+    [InlineData(0)]
+    public void IndexerGetBounds(int index) => wrapper.List.Invoking(l => l[index]).Should().Throw<ArgumentOutOfRangeException>();
 
-        l.Invoking(l => l[index] = "").Should().Throw<ArgumentOutOfRangeException>();
+    [Fact]
+    public void IndexerGet()
+    {
+        g.Assert(s, p, g.AssertList([o]));
+
+        wrapper.List[0].Should().Be(v);
     }
 
     [Fact]
-    public void AddNull()
-    {
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
+    public void IndexerSetNull() => wrapper.List.Invoking(l => l[0] = null!).Should().Throw<ArgumentNullException>();
 
-        l.Invoking(l => l.Add(null!)).Should().Throw<ArgumentNullException>();
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void IndexerSetBounds(int index) => wrapper.List.Invoking(l => l[index] = "").Should().Throw<ArgumentOutOfRangeException>();
+
+    [Theory]
+    [MemberData(nameof(Counts))]
+    public void Count(IEnumerable<INode> objects)
+    {
+        g.Assert(s, p, g.AssertList(objects));
+
+        wrapper.List.Should().HaveCount(objects.Count());
+    }
+
+    public static IEnumerable<object[]> Counts => [
+        [ Array.Empty<INode>() ],
+        [ new INode[] { s } ],
+        [ new INode[] { s, p } ],
+        [ new INode[] { s, p, o } ],
+    ];
+
+    [Fact]
+    public void IsReadOnly() => wrapper.List.IsReadOnly.Should().BeFalse();
+
+    [Fact]
+    public void AddNull() => wrapper.List.Invoking(l => l.Add(null!)).Should().Throw<ArgumentNullException>();
+
+    [Fact]
+    public void AddToNothing()
+    {
+        var expected = new Graph();
+        expected.Assert(s, p, expected.AssertList([o]));
+
+        wrapper.List.Add(v);
+
+        g.Should().BeIsomorphicWith(expected);
     }
 
     [Fact]
-    public void ContainsNull()
+    public void AddToEmpty()
     {
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
+        g.Assert(s, p, g.AssertList([]));
 
-        l.Invoking(l => l.Contains(null!)).Should().Throw<ArgumentNullException>();
+        var expected = new Graph();
+        expected.Assert(s, p, expected.AssertList([o]));
+
+        wrapper.List.Add(v);
+
+        g.Should().BeIsomorphicWith(expected);
     }
 
     [Fact]
-    public void IndexOfNull()
+    public void AddToList()
     {
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
+        g.Assert(s, p, g.AssertList([o]));
 
-        l.Invoking(l => l.IndexOf(null!)).Should().Throw<ArgumentNullException>();
+        var expected = new Graph();
+        expected.Assert(s, p, expected.AssertList([o, o,s]));
+
+        wrapper.List.Add(v);
+
+        g.Should().BeIsomorphicWith(expected);
     }
 
     [Fact]
-    public void InsertNull()
+    public void ClearNothing()
     {
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
+        g.LoadFromString("[ a 0 ] .", new TurtleParser());
+    
+        var expected = new Graph();
+        expected.LoadFromString("[ a 0 ] .", new TurtleParser());
 
-        l.Invoking(l => l.Insert(0, null!)).Should().Throw<ArgumentNullException>();
+        wrapper.List.Clear();
+
+        g.Should().BeIsomorphicWith(expected);
     }
+
+    [Fact]
+    public void ClearEmpty()
+    {
+        g.LoadFromString("[ a 0 ] . <http://example.com/s> <http://example.com/p> () .", new TurtleParser());
+
+        var expected = new Graph();
+        expected.LoadFromString("[ a 0 ] . <http://example.com/s> <http://example.com/p> () .", new TurtleParser());
+
+        wrapper.List.Clear();
+
+        g.Should().BeIsomorphicWith(expected);
+    }
+
+    [Fact]
+    public void ClearList()
+    {
+        g.LoadFromString("[ a 0 ] . <http://example.com/s> <http://example.com/p> (0) .", new TurtleParser());
+
+        var expected = new Graph();
+        expected.LoadFromString("[ a 0 ] . <http://example.com/s> <http://example.com/p> () .", new TurtleParser());
+
+        wrapper.List.Clear();
+
+        g.Should().BeIsomorphicWith(expected);
+    }
+
+    [Fact]
+    public void ContainsNull() => wrapper.List.Invoking(l => l.Contains(null!)).Should().Throw<ArgumentNullException>();
+
+    [Fact]
+    public void ContainsNothing() => wrapper.List.Should().NotContain(v);
+
+    [Fact]
+    public void ContainsEmpty()
+    {
+        g.Assert(s, p, g.AssertList([]));
+
+        wrapper.List.Should().NotContain(v);
+    }
+
+    [Fact]
+    public void ContainsMismatch()
+    {
+        g.Assert(s, p, g.AssertList([s]));
+
+        wrapper.List.Should().NotContain(v);
+    }
+
+    [Fact]
+    public void Contains()
+    {
+        g.Assert(s, p, g.AssertList([o]));
+
+        wrapper.List.Should().Contain(v);
+    }
+
+    [Fact]
+    public void IndexOfNull() => wrapper.List.Invoking(l => l.IndexOf(null!)).Should().Throw<ArgumentNullException>();
+
+    [Fact]
+    public void InsertNull() => wrapper.List.Invoking(l => l.Insert(0, null!)).Should().Throw<ArgumentNullException>();
 
     [Theory]
     [InlineData(-1, true)]
@@ -83,9 +183,7 @@ public class RdfCollectionListTest
     [InlineData(1, true)]
     public void InsertBounds(int index, bool throws)
     {
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
-        var inserting = l.Invoking(l => l.Insert(index, ""));
+        var inserting = wrapper.List.Invoking(l => l.Insert(index, ""));
 
         if (throws)
         {
@@ -97,246 +195,16 @@ public class RdfCollectionListTest
         }
     }
 
+    [Fact]
+    public void RemoveNull() => wrapper.List.Invoking(l => l.Remove(null!)).Should().Throw<ArgumentNullException>();
 
+    [Theory]
+    [InlineData(-1)]
+    [InlineData(0)]
+    public void RemoveAtBounds(int index) => wrapper.List.Invoking(l => l.RemoveAt(index)).Should().Throw<ArgumentOutOfRangeException>();
 
-
-
-
-
-
-
-
-
-
-
-
-    //[Fact]
-    public void EmptyWithNoTriple()
+    class SampleWrapper(INode node, IGraph graph) : GraphWrapperNode(node, graph)
     {
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
-
-        l.Should().BeEmpty();
-        g.IsEmpty.Should().BeTrue();
-    }
-
-    //[Fact]
-    public void AssertsWithNoTriple()
-    {
-        var expected = new Graph();
-        expected.LoadFromString("<http://example.com/s> <http://example.com/p1> ('o') .", new TurtleParser());
-
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
-
-        l.Add("o");
-
-        g.Equals(expected).Should().BeTrue();
-    }
-
-    //[Fact]
-    public void List1()
-    {
-        var g = new Graph();
-        g.LoadFromString("<http://example.com/s> <http://example.com/p1> () .", new TurtleParser());
-        var l = SampleWrapperResource.Wrap(s, g).List;
-
-        l.Should().BeEmpty();
-    }
-
-    //[Fact]
-    public void List2()
-    {
-        var g = new Graph();
-        g.LoadFromString("<http://example.com/s> <http://example.com/p1> ('o') .", new TurtleParser());
-        var l = SampleWrapperResource.Wrap(s, g).List;
-
-        l.Should().BeEquivalentTo(["o"]);
-    }
-
-    //[Fact]
-    public void Add1()
-    {
-        var expected = new Graph();
-        expected.LoadFromString("<http://example.com/s> <http://example.com/p1> ('o') .", new TurtleParser());
-
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g).List;
-
-        l.Add("o");
-
-        g.Equals(expected).Should().BeTrue();
-    }
-
-    //[Fact]
-    public void Add2()
-    {
-        var expected = new Graph();
-        expected.LoadFromString("<http://example.com/s> <http://example.com/p1> ('o') .", new TurtleParser());
-
-        var g = new Graph();
-        g.LoadFromString("<http://example.com/s> <http://example.com/p1> () .", new TurtleParser());
-        var l = SampleWrapperResource.Wrap(s, g).List;
-
-        l.Add("o");
-
-        g.Equals(expected).Should().BeTrue();
-    }
-
-    //[Fact]
-    public void Add3()
-    {
-        var expected = new Graph();
-        expected.LoadFromString("<http://example.com/s> <http://example.com/p1> ('o' 'o2') .", new TurtleParser());
-
-        var g = new Graph();
-        g.LoadFromString("<http://example.com/s> <http://example.com/p1> ('o') .", new TurtleParser());
-        var l = SampleWrapperResource.Wrap(s, g).List;
-
-        l.Add("o2");
-
-        g.Equals(expected).Should().BeTrue();
-    }
-
-    //[Fact]
-    public void WrapperList1()
-    {
-        var expected = new Graph();
-        expected.LoadFromString("<http://example.com/s> <http://example.com/p1> (<http://example.com/s>) .", new TurtleParser());
-
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g);
-
-        l.WrapperList.Add(l);
-
-        g.Equals(expected).Should().BeTrue();
-    }
-
-    //[Fact]
-    public void WrapperList2()
-    {
-        var g = new Graph();
-        g.LoadFromString("<http://example.com/s> <http://example.com/p1> ( [ <http://example.com/p1> 'o' ] ) .", new TurtleParser());
-        var l = SampleWrapperResource.Wrap(s, g);
-
-        l.WrapperList.Select(x => x.MyProperty1).Single().Should().Be("o");
-    }
-
-    //[Fact]
-    public void ListObjects3()
-    {
-        var g = new Graph();
-        g.LoadFromString("<http://example.com/s> <http://example.com/p1> ( 'o1' 'o2' ), (), ( 'o3' 'o4' ) .", new TurtleParser());
-        var l = SampleWrapperResource.Wrap(s, g);
-
-        l.ListObjects.SelectMany(x => x).Should().BeEquivalentTo(["o1", "o2", "o3", "o4"]);
-    }
-
-    //[Fact]
-    public void ListObjects4()
-    {
-        var g = new Graph();
-        g.LoadFromString("<http://example.com/s> <http://example.com/p1> () .", new TurtleParser());
-        var l = SampleWrapperResource.Wrap(s, g);
-
-        l.ListObjects.SelectMany(x => x).Should().BeEmpty();
-    }
-
-    //[Fact]
-    public void ListObjects5()
-    {
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g);
-
-        l.ListObjects.SelectMany(x => x).Should().BeEmpty();
-    }
-
-    //[Fact]
-    public void ListObjects6()
-    {
-        var expected = new Graph();
-        expected.LoadFromString("<http://example.com/s> <http://example.com/p1> ( 'o' ) .", new TurtleParser());
-
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g);
-
-        l.ListObjects.Add(["o"]);
-
-        g.Equals(expected).Should().BeTrue();
-    }
-
-    //[Fact]
-    public void ListObjects7()
-    {
-        var expected = new Graph();
-        expected.LoadFromString("<http://example.com/s> <http://example.com/p1> ( 'o1' 'o2' ) .", new TurtleParser());
-
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g);
-
-        l.ListObjects.Add(["o1"]);
-        l.ListObjects.First().Add("o2");
-
-        g.Equals(expected).Should().BeTrue();
-    }
-
-    //[Fact]
-    public void ListObjects8()
-    {
-        var expected = new Graph();
-        expected.LoadFromString("<http://example.com/s> <http://example.com/p1> ( 'o1' 'o2' ), ( 'o3' 'o4' ) .", new TurtleParser());
-
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g);
-
-        l.ListObjects.Add(["o1"]);
-        l.ListObjects.First().Add("o2");
-        l.ListObjects.Add(["o3"]);
-        l.ListObjects.Last().Add("o4");
-
-        g.Equals(expected).Should().BeTrue();
-    }
-
-    //[Fact]
-    public void ListList1()
-    {
-        var expected = new Graph();
-        expected.LoadFromString("<http://example.com/s> <http://example.com/p1> ( ( 'o1' 'o' ) ( 'o2' 'o' )  ) .", new TurtleParser());
-
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g);
-
-        l.ListList.Add(["o1"]);
-        l.ListList.Add(["o2"]);
-
-        foreach (var list in l.ListList)
-        {
-            list.Add("o");
-        }
-
-        g.Equals(expected).Should().BeTrue();
-    }
-
-    //[Fact]
-    public void ListListList1()
-    {
-        var expected = new Graph();
-        expected.LoadFromString("<http://example.com/s> <http://example.com/p1> ( ( ( 'o1' 'o2' 'o' ) ( 'o3' 'o4' 'o' ) ) ( ( 'o5' 'o6' 'o' ) ( 'o7' 'o8' 'o' ) ) ) .", new TurtleParser());
-
-        var g = new Graph();
-        var l = SampleWrapperResource.Wrap(s, g);
-
-        l.ListListList.Add([["o1", "o2"], ["o3", "o4"]]);
-        l.ListListList.Add([["o5", "o6"], ["o7", "o8"]]);
-
-        foreach (var listlist in l.ListListList)
-        {
-            foreach (var list in listlist)
-            {
-                list.Add("o");
-            }
-        }
-
-        g.Equals(expected).Should().BeTrue();
+        internal IList<string> List => this.List(p, NodeMappings.From, ValueMappings.As<string>);
     }
 }

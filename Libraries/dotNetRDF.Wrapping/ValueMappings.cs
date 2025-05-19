5db21f88014243a777ed3d6f9072750a2f29706f
Wojciech Stawiarski <wojtek@egosum.pl>
2025-05-19 15:03:46 +0100

    Enum value mappings

--- Libraries/dotNetRDF.Wrapping/ValueMappings.cs
+++ Libraries/dotNetRDF.Wrapping/ValueMappings.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
+using VDS.RDF.Nodes;
 
 namespace VDS.RDF.Wrapping;
 
@@ -18,6 +19,12 @@ public static class ValueMappings
 
     public static KeyValuePair<CultureInfo, string> AsLangStringPair(GraphWrapperNode? node) => new KeyValuePair<CultureInfo, string>(
         CultureInfo.GetCultureInfo((node as ILiteralNode).Language), (node as ILiteralNode).Value);
+
+    public static T ToEnum<T>(GraphWrapperNode? node) where T : Enum => (T)Enum.ToObject(typeof(T), node.AsValuedNode().AsInteger());
+    
+    public static T EnumFromName<T>(GraphWrapperNode? node) where T : Enum => (T)Enum.Parse(typeof(T), (node as ILiteralNode).Value);
+    
+    public static ValueMapping<T> EnumFromUri<T>(string prefix) where T : Enum => node => (T)Enum.Parse(typeof(T), new Uri(prefix).MakeRelativeUri((node as IUriNode).Uri).ToString());
 
     public static ValueMapping<IList<T>> AsList<T>(GraphWrapperNode subject, INode predicate, NodeMapping<T> nmap, ValueMapping<T> vmap) =>
         node => new RdfCollectionList<T>(node, subject, predicate, nmap, vmap);

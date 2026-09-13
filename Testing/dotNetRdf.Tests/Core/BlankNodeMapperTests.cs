5395ddb7f7069e00037281b65a6538181807767c
Samu Lang <langsamu@hotmail.com>
2026-09-13 15:17:39 +0100

    Cover current state

---
+++ Testing/dotNetRdf.Tests/Core/BlankNodeMapperTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace VDS.RDF;
+
+public class BlankNodeMapperTests
+{
+    private const string GivenPrefix = "mine";
+
+    [Fact]
+    public void DefaultPrefixUsed() =>
+        new BlankNodeMapper().GetNextID().Should().StartWith("autos");
+
+    [Fact]
+    public void GivenPrefixNotUsed() =>
+        new BlankNodeMapper(GivenPrefix).GetNextID().Should().StartWith("autos");
+
+    [Fact]
+    public void NullPrefixDoesNotThrow() =>
+        new BlankNodeMapper(null).GetNextID().Should().StartWith("autos");
+
+    [Fact]
+    public void EmptyPrefixDoesNotThrow() =>
+        new BlankNodeMapper(string.Empty).GetNextID().Should().StartWith("autos");
+
+    [Fact]
+    public void WhitespacePrefixDoesNotThrow() =>
+        new BlankNodeMapper(" ").GetNextID().Should().StartWith("autos");
+}
